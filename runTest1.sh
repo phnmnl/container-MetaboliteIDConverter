ec44eb002cec0c73892fdf2cb6087c627b2c0ae0
bmerlet90 <benjimerlet@gmail.com>
2016-11-15 10:17:08 +0000

    modified test script

--- runTest1.sh
+++ runTest1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get any dependencies needed for the testing part
-apt-get update && apt-get install -y --no-install-recommends wget
+# apt-get update && apt-get install -y --no-install-recommends wget
 
 # Check that the JUnit test classes pass on the built container
 
@@ -20,10 +20,10 @@ mkdir /tmp/study-dir
 # Download Test data 
 wget http://vm-metexplore-dev.toulouse.inra.fr:3000/bmerlet/parsebionet-Enrichment/src/master/test-data/input.tsv
 
-mv input.tsv /tmp/study-dir/
+cp input.tsv /tmp/study-dir/
 
 # Run the actual app on the test data
-java -jar /Javafiles/dist/phnmnl-enrichment/phnmnl-enrichment.jar -inDB KEGG -inFile /tmp/study-dir/input.tsv -outFile /tmp/study-dir/out.tsv -headers
+java -jar /Javafiles/dist/phnmnl-enrichment/phnmnl-enrichment.jar -inDB "KEGG" -inFile "/tmp/study-dir/input.tsv" -outFile "/tmp/study-dir/out.tsv" -headers
 
 # Check that the output file is created/correctness
 if ! [ -e "/tmp/study-dir/out.tsv" ]; then
