59b7e7089e2ce792cc75229a6ea4707ac02f4198
bmerlet90 <benjimerlet@gmail.com>
2016-11-15 13:31:58 +0000

    testing modification of test script

--- runTest1.sh
+++ runTest1.sh
@@ -20,7 +20,7 @@ mkdir /tmp/study-dir
 # Download Test data 
 wget http://vm-metexplore-dev.toulouse.inra.fr:3000/bmerlet/parsebionet-Enrichment/src/master/test-data/input.tsv
 
-chmod +r input.tsv
+chmod 777 input.tsv
 
 # Run the actual app on the test data
 java -jar /Javafiles/dist/phnmnl-enrichment/phnmnl-enrichment.jar -inDB "KEGG" -inFile input.tsv -outFile "/tmp/study-dir/out.tsv" -headers
