27fb7bad310e9c384641e4d5e7f48331a5380f7d
bmerlet90 <benjimerlet@gmail.com>
2016-11-15 14:23:22 +0000

    modified wget link to download correct file

--- runTest1.sh
+++ runTest1.sh
@@ -21,11 +21,11 @@ Code=0
 mkdir /tmp/study-dir 
 
 # Download Test data 
-wget http://vm-metexplore-dev.toulouse.inra.fr:3000/bmerlet/parsebionet-Enrichment/src/master/test-data/input.tsv
+wget http://vm-metexplore-dev.toulouse.inra.fr:3000/bmerlet/parsebionet-Enrichment/raw/develop/test-data/input.tsv
 
-chmod 777 input.tsv
+# chmod 777 input.tsv
 
-echo "$(cat input.tsv)"
+# echo "$(cat input.tsv)"
 
 # Run the actual app on the test data
 cmd="java -jar /Javafiles/dist/phnmnl-enrichment/phnmnl-enrichment.jar -inDB \"KEGG\" -headers -inFile ./input.tsv -outFile /tmp/study-dir/out.tsv"
